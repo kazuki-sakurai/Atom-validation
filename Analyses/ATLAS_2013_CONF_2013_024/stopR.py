b71dc56a7a2bed5af693ba2794a93096b1791db3
kazuki-sakurai <kazuki.sakurai.1225@gmail.com>
2014-03-19 20:25:12 +0000

    added ATLAS_2013_CONF_2013_024

---
+++ Analyses/ATLAS_2013_CONF_2013_024/stopR.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python
+
+__author__ = "Kazuki Sakurai <kazuki.sakurai@kcl.ac.uk>"
+__version__ = "0.1"
+
+import sys
+import os
+from math import *
+sys.path.append('../..')
+from functions import *
+
+vname = 'stopR'
+
+if __name__ == '__main__':
+    inputfile = sys.argv[1]
+
+    ananame, eff_dict, err_dict, pid = GetEfficiencies(inputfile)
+
+    Ntot_exp = 250000.
+    per = 100.
+    
+    eff_dict['No-cut'] = 1.
+    err_dict['No-cut'] = 0.
+
+    initial_list = [ # ATLAS results: number is scalled for the 600 stop xsec with 20.5/fb
+                    ['No-cut',                        507.3],
+                    ['Muon veto',                     381.2],
+                    ['Electron veto',                 284.1],
+                    ['MET > 130',                     263.1],
+                    ['Jet multiplicity and pT',        97.3],
+                    ['MET_track > 30',                 96.3],
+                    ['delPhi(MET, MET_track) < pi/3',  90.3],
+                    ['delPhi(jet, MET) > pi/5',        77.1],
+                    ['Tau veto',                       67.4],
+                    ['mT(bjet, MET) > 175',            29.5],
+                    ['80 < m^0_jjj < 270',             17.8],                    
+                    ['80 < m^1_jjj < 270',             10.9],                    
+                    ['SR1: MET > 200',                 10.3],                    
+                    ['SR2: MET > 300',                  7.8],                    
+                    ['SR3: MET > 350',                  6.1]]
+
+    #######################################################
+    #          Setting experimental efficiencies          #  
+    #######################################################
+    name_list = []
+    eff_exp = []
+    err_exp = []
+    nev_exp = []
+    for name, inival in initial_list:
+        name_list.append(name)
+        eff = inival/initial_list[0][1]
+        nev = Ntot_exp * eff
+        nerr = sqrt(nev)
+        eff_err = nerr/Ntot_exp
+
+        eff_exp.append(per * eff)
+        err_exp.append(per * eff_err)
+        nev_exp.append(nev)
+
+    #######################################################
+    #          Setting experimental efficiencies          #  
+    #######################################################
+    eff_atom = []
+    err_atom = []
+    ratio_eff = ['']
+    ratio_eff_sig = ['']
+    for i in range(len(name_list)):          
+        name = name_list[i]
+        eff = eff_dict[name]
+        err = err_dict[name]
+        eff_atom.append(per * eff)
+        err_atom.append(per * err)
+        if i > 0:
+            ratio_eff.append( eff_atom[i]/eff_exp[i] )
+            err = sqrt(err_atom[i]**2 + err_exp[i]**2)
+            ratio_eff_sig.append( (err_atom[i] - err_exp[i])/err )
+
+    #######################################################
+    #          Setting relative efficiencies              #  
+    #######################################################
+    Reff_exp = ['']
+    Rerr_exp = ['']
+    Reff_atom = ['']
+    Rerr_atom = ['']
+    ratio_R = ['']
+    ratio_R_sig = ['']    
+    for i in range(1, len(name_list)):      
+        nev = nev_exp[i]
+        prevnev = nev_exp[i - 1]
+        Reff_exp.append(nev/prevnev)
+        Rerr_exp.append(sqrt(nev)/prevnev)
+
+        eff = eff_atom[i]
+        err = eff_atom[i]        
+        preveff = eff_atom[i - 1]
+        Reff_atom.append(eff/preveff)
+        Rerr_atom.append(err/preveff)
+
+        ratio_R.append( Rerr_atom[i]/Rerr_exp[i] )
+        err = sqrt(Rerr_exp[i]**2 + Rerr_atom[i]**2)
+        ratio_R_sig.append( (Rerr_atom[i] - Rerr_exp[i])/err )
+
+
+    #######################################################
+    #                       Display                       #  
+    #######################################################
+    show_cutflow(ananame, vname, 'ATLAS', name_list, 
+                 eff_exp, err_exp, eff_atom, err_atom, ratio_eff, ratio_eff_sig, 
+                 Reff_exp, Rerr_exp, Reff_atom, Rerr_atom, ratio_R, ratio_R_sig)    
+
+
+
+
--- setting.sh
+++ setting.sh
@@ -12,13 +12,13 @@ get_names ()
     ana=$1
     vname=()
     fname=()
+
     if [[ $ana == "ATLAS_2013_CONF_2013_024" ]]; then
 
         vname_list+=("stopL")
-        fname_list+=($ana"/V024_LHC8_T1tN1_100left_600-1.hepmc")
+        fname_list+=("V024_LHC8_T1tN1_100left_600-1.hepmc")
 
         vname_list+=("stopR")
-        fname_list+=($ana"/V024_LHC8_T1tN1_95right_600-1.hepmc")
-
+        fname_list+=("V024_LHC8_T1tN1_95right_600-1.hepmc")
     fi
 }
