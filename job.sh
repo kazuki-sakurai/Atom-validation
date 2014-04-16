a60a9a8241ddae40ab8cc576adeeb447e237bba9
kazuki-sakurai <kazuki.sakurai.1225@gmail.com>
2014-04-16 22:40:51 +0200

    move *.root to backup b4 run

--- job.sh
+++ job.sh
@@ -9,6 +9,10 @@ ana=ANA; vname=VNAME; fname=FNAME
 event_path=$wkdir/../Validation-events/$ana
 
 cd $wkdir/Analyses/$ana
+if [[ ! -d backup ]]; then
+    mkdir backup
+fi
+mv $vname.root backup/
 atom -a $ana $event_path/$fname && mv atom.yoda.root $vname.root #&& mv atom.yoda.yoda $vname.yoda
 
 ######################################################
